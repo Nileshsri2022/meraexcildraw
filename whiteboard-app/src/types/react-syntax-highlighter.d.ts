25c24938c4e793427b6dfa8b4b342aa7be6a9b58
Nileshsri2022 <2022021140@mmmut.ac.in>
2026-02-26 20:26:03 +0530

    fix-npx build

--- whiteboard-app/src/types/react-syntax-highlighter.d.ts
+++ whiteboard-app/src/types/react-syntax-highlighter.d.ts
@@ -1,13 +1,11 @@
 declare module "react-syntax-highlighter/dist/esm/prism-light" {
     import { PrismLight } from "react-syntax-highlighter";
-    export { PrismLight };
     export default PrismLight;
 }
 
-declare module "react-syntax-highlighter/dist/esm/styles/prism" {
-    export { default as oneDark } from "react-syntax-highlighter/dist/esm/styles/prism/one-dark";
-    const styles: Record<string, Record<string, React.CSSProperties>>;
-    export default styles;
+declare module "react-syntax-highlighter/dist/esm/styles/prism/one-dark" {
+    const style: Record<string, React.CSSProperties>;
+    export default style;
 }
 
 declare module "react-syntax-highlighter/dist/esm/languages/prism/*" {
